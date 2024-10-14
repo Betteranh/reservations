3ebe31c363ae84e699ac55d6356f2d5f21840e23
Betteranh <danguyen.1129@gmail.com>
2024-10-14 22:53:48 +0200

    After framework installation

---
+++ pid-reservations/src/main/java/com/icc/pid_reservations/PidReservationsApplication.java
@@ -0,0 +1,13 @@
+package com.icc.pid_reservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PidReservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PidReservationsApplication.class, args);
+	}
+
+}
---
+++ pid-reservations/src/test/java/com/icc/pid_reservations/PidReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icc.pid_reservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PidReservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
